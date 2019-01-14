a5ee8c0ba40dbdebe78589f0e18049a56a898915
Zachary LaVere <zachary.lavere@gmail.com>
2019-01-14 10:56:12 -0500

    added to about me

--- index.php
+++ index.php
@@ -4,6 +4,13 @@ echo start();
 ?>
 <div class="container">
         <h2>About Me</h2>
+        <p>Zachary LaVere born in West Palm Beach, Florida. Moved to Flowery Branch, Georgia in 1998.
+            Attended West Hall High School - class of 2007. Went to University of West Georgia to pursue
+            a Bachelor's degree in Psychology. Received a BA in Psychology in Spring of 2011. Went on to pursue MA
+            in Psychology in Fall of 2011. Graduated with an MA in Psychology in December of 2014. </p>
+        <p>Began work in UWG's Office of Undergraduate Admissions as a Departmental IT Specialist in April of 2015.
+            Started BS in Computer Science in Fall of 2015. Transitioned to UWG's Information Technology Services as an
+            Applications Support Specialist in June of 2018</p>
 </div>
 
 <?php
