b7a4a276d87c88996db3516e8996be3ba0731bb2
Zachary LaVere <zachary.lavere@gmail.com>
2019-01-29 15:39:27 -0500

    Modified main page content

--- index.php
+++ index.php
@@ -14,11 +14,7 @@ echo start();
         </ul>
         <h3>Professional Experience</h3>
         <ul>
-            <li>Began working for K.B. Fishbones at 15</li>
-            <li>Started working Summers at International Furniture Industries (IFI)</li>
-            <li>At 18, worked at Checkers for 1 Summer, before attending University of West Georgia</li>
-            <li>Worked at Wal-mart for 1 Summer while pursuing undergraduate degree</li>
-            <li>Became Graduate Assistant to Psychology department chair Dr. Donadrian Rice.</li>
+           <li>Graduate Assistant to Psychology department chair Dr. Donadrian Rice.</li>
             <ul><li>Led team to entirely rework the Department of Psychology's website.</li></ul>
             <li>In April of 2015, started work as Departmental IT Specialist for UWG's Office of Undergraduate Admissions</li>
             <ul>
@@ -29,7 +25,14 @@ echo start();
                     Managed Admission Application and Office's Website.
                 </li>
             </ul>
-            <li>Present: June 2018, accepted a position as Application Support Specialist in the Department of Enterprise Applications and Development.</li>
+            <li>Present: June 2018, accepted a position as Systems Support Specialist in the Department of Enterprise Applications and Development.</li>
+            <ul>
+                <li>Support 200+ Active Applications at the University of West Georgia</li>
+                <li>Developed API Connector for Greenfield Data Analysis: MS SQL, MySQL, Oracle SQL, Dynamics API</li>
+                <li>Data Modeling: Student Enrollment, Retention, Progression, and Graduation</li>
+                <li>Data Warehousing for student data modeling and analysis</li>
+                <li>Data Science for University operations in student affairs and enrollment management</li>
+            </ul>
         </ul>
 
         <h3>Education</h3>
@@ -40,7 +43,7 @@ echo start();
             <li>Master of Arts in Psychology in Fall of 2011</li>
             <li>Graduated with MA in Psychology in Fall of 2014</li>
             <li>Bachelor of Science in Computer Science in Fall of 2015</li>
-            <li>Expected Graduation with BS in Computer Science in Fall of 2020</li>
+            <li>Expected Graduation with Bachelor of Science in Computer Science in Fall of 2020</li>
         </ul>
         <h3>Programming Languages</h3>
         <ul>
